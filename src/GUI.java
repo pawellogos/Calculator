5e04f13abe76602ed9a63c3e2326e0e81205ceec
Pawel <pawel.logo@gmail.com>
2025-03-06 13:23:10 +0100

    mod_1

--- src/GUI.java
+++ src/GUI.java
@@ -1,10 +1,17 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI extends JFrame {
+
+public class GUI extends JFrame implements ActionListener{
+    public CalculatorLogic logic = new CalculatorLogic();
     public static final int WIDTH = 350;
     public static final int HEIGHT = 400;
+    JTextField input = new JTextField();
+    JTextField output = new JTextField();
+
     public static final String[] buttons = {"%","CE","C","back","1/x","x^2","sqrt(x)","/","7","8","9","*","4","5","6","-","1","2","3","+","+/-","0",".","="};
     public GUI(){
         setTitle("Calculator");
@@ -19,8 +26,6 @@ public class GUI extends JFrame {
 
         //Middle section - TextFields for inputs and output
         JPanel mid = new JPanel(new GridLayout(2,1));
-        JTextField input = new JTextField();
-        JTextField output = new JTextField();
         input.setEditable(false);
         output.setEditable(false);
         mid.add(input,output);
@@ -30,14 +35,27 @@ public class GUI extends JFrame {
         for (String button : buttons){
             JButton but = new JButton(button);
             bottom.add(but);
+            but.addActionListener(this);
         }
 
         //Add all sections to the frame
         add(label_button,BorderLayout.NORTH);
         add(mid,BorderLayout.CENTER);
         add(bottom,BorderLayout.SOUTH);
-
         setVisible(true);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        switch (command) {
+            case "C" -> input.setText("test");
+            //case "9" -> input.setText(input.getText() + "testttt");
+            default -> {
+                if (command.matches("\\d")){ //(0-9)
+                    input.setText(input.getText() + command);
+                }
+            }
+        }
     }
 }
 
